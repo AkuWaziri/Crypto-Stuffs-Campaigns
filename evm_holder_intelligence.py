1746890d88249d3b837c99b2d78b3f7308a3d356
Aku Waziri <32820229+AkuWaziri@users.noreply.github.com>
2026-09-11 17:43:28 +0100

    Fix ERC-20 Transfer event topic

--- evm_holder_intelligence.py
+++ evm_holder_intelligence.py
@@ -25,7 +25,7 @@ class HolderValidationError(HolderIntelligenceError):
     pass
 
 
-TRANSFER_TOPIC = "0x" + "ddf252ad" + ("0" * 56)
+TRANSFER_TOPIC = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
 ZERO_ADDRESS = "0x" + ("0" * 40)
 
 
