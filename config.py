9afae88666e694cd57ae71d02d31141c9dfee522
Aku Waziri <32820229+AkuWaziri@users.noreply.github.com>
2026-09-23 16:39:45 +0100

    Add configurable X SearchTimeline query ID

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ MAX_FEED_ITEMS = int(os.getenv("MAX_FEED_ITEMS", "10"))
 LOOKBACK_HOURS = int(os.getenv("LOOKBACK_HOURS", "36"))
 ENABLE_WEB_SOURCES = os.getenv("ENABLE_WEB_SOURCES", "true").lower() == "true"
 USER_AGENT = os.getenv("USER_AGENT", "Crypto-Stuffs-Campaigns/1.0")
+X_SEARCH_QUERY_ID = os.getenv("X_SEARCH_QUERY_ID", "")
