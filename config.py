9e1eb78c43beb58a6c9475946cee2b4207a4bd2f
Aku Waziri <32820229+AkuWaziri@users.noreply.github.com>
2026-09-23 15:43:43 +0100

    Build Crypto-Stuffs-Campaigns

--- config.py
+++ config.py
@@ -2,7 +2,10 @@ import os
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
-HELIUS_API_KEY = os.getenv("HELIUS_API_KEY", "")
-EVM_RPC_URL = os.getenv("EVM_RPC_URL", "")
-
-READ_ONLY = True
+X_AUTH_TOKEN = os.getenv("X_AUTH_TOKEN", "")
+X_CT0 = os.getenv("X_CT0", "")
+X_SEARCH_LIMIT = int(os.getenv("X_SEARCH_LIMIT", "30"))
+MAX_FEED_ITEMS = int(os.getenv("MAX_FEED_ITEMS", "10"))
+LOOKBACK_HOURS = int(os.getenv("LOOKBACK_HOURS", "36"))
+ENABLE_WEB_SOURCES = os.getenv("ENABLE_WEB_SOURCES", "true").lower() == "true"
+USER_AGENT = os.getenv("USER_AGENT", "Crypto-Stuffs-Campaigns/1.0")
